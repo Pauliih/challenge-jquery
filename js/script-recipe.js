0b36c430ead61c5b926a4fb4ce20d922e0b93409
Pauliih <pau.campos89@gmail.com>
2018-01-08 17:33:55 -0300

    Etapa 8: agregar y quitar clase make al div de clase page

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-	$('.js-menu').hide();
+  $('.js-menu').hide();
 
-	console.log("Está listo el HTML");
+  console.log("Está listo el HTML");
+
+  $('.js-show-make').click(function(){
+    $('.page').addClass('make');
+  })
+
+  $('.js-show-recipe').click(function(){
+    $('.page').removeClass('make');
+  })
 })
