a4626b5468f2e230090f33c0a35289fb2eae4c99
Pauliih <pau.campos89@gmail.com>
2018-01-08 17:40:11 -0300

    Etapa 9: agregamos y quitamos la clase active a los a con clase js-show-recipe js-show-make

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -5,9 +5,13 @@ $(document).ready(function(){
 
   $('.js-show-make').click(function(){
     $('.page').addClass('make');
+    $('.js-show-make').addClass('active');
+    $('.js-show-recipe').removeClass('active');
   })
 
   $('.js-show-recipe').click(function(){
     $('.page').removeClass('make');
+    $('.js-show-recipe').addClass('active');
+    $('.js-show-make').removeClass('active');
   })
 })
